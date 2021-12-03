52bcb21e93d2a1b18ee3a5b875d689a722121fef
M45TR0 <m4st3r0w@gmail.com>
2021-12-03 04:50:41 +0100

    Day 1 part 2 complete

--- src/Day1part2.java
+++ src/Day1part2.java
@@ -2,27 +2,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Day2{
+public class Day1part2 {
 
-    public static void main(String[] args) throws FileNotFoundException {
+public static void main(String[] args) throws FileNotFoundException {
         int count = 0;
         Scanner input = new Scanner(new File("./src/input.txt"));
         int comp1 = Integer.parseInt(input.nextLine());
         int comp2 = Integer.parseInt(input.nextLine());
         int comp3 = Integer.parseInt(input.nextLine());
         while(input.hasNextLine()){
-            int comp4 = Integer.parseInt(input.nextLine());
-            int calc = comp1 + comp2 + comp3;
-            int calc2 = comp2 + comp3 + comp4;
-            if (calc2 > calc){
-                count++;
-            }
-            comp1 = comp2;
-            comp2 = comp3;
-            comp3 = comp4;
+        int comp4 = Integer.parseInt(input.nextLine());
+        int calc = comp1 + comp2 + comp3;
+        int calc2 = comp2 + comp3 + comp4;
+        if (calc2 > calc){
+        count++;
+        }
+        comp1 = comp2;
+        comp2 = comp3;
+        comp3 = comp4;
         }
         System.out.println(count);
 
-    }
-}
-
+        }
+        }
