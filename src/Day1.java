cb83f3b82a2f03a7b223f0cb6fa31b1e0297f42f
M45TR0 <m4st3r0w@gmail.com>
2021-12-03 04:20:20 +0100

    test

---
+++ src/Day1.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1{
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int count = 0;
+        Scanner input = new Scanner(new File("./src/input.txt"));
+        int comp1 = Integer.parseInt(input.nextLine());
+        while(input.hasNextLine()){
+            int comp2 = Integer.parseInt(input.nextLine());
+            if (comp1 < comp2) {
+                count++;
+            }
+            comp1 = comp2;
+
+        }
+        System.out.println(count);
+    }
+}
+
